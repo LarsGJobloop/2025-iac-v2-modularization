c5d89ea7cf643b7cc17eca0f8b80f49b2a9ac866
zabronax <zabronax@gmail.com>
2025-04-29 09:48:20 +0200

    setup ssh key

--- infrastructure/environment/production/main.tf
+++ infrastructure/environment/production/main.tf
@@ -1,8 +1,15 @@
+resource "hcloud_ssh_key" "main" {
+  name       = "project-ssh-key"
+  public_key = file("./ida_rsa.pub")
+}
+
 resource "hcloud_server" "web_app" {
   name = "web-app"
   server_type = "cax21"
   image = "debian-12"
   location = "hel1"
+
+  ssh_keys = [ hcloud_ssh_key.main.id ]
 
   public_net {
     ipv4_enabled = true
